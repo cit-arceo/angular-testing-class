9b2993d07481a208736b9140a196c2c3388b34f3
cit-arceo <citlalliarceo@gmail.com>
2020-10-19 11:44:31 -0500

    add currencies-demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ComputeDemoComponent } from '.compute/compute-demo/compute-demo.component';
 import { GreetDemoComponent } from './greet/greet-demo/greet-demo.component';
+import { CurrenciesDemoComponent } from './currencies-demo/currencies-demo.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ComputeDemoComponent
-    GreetDemoComponent
+    GreetDemoComponent,
+    CurrenciesDemoComponent
   ],
   imports: [
     BrowserModule
